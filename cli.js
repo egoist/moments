429a794e3087f326789434d1f74edde367534957
egoist <aprilorange.net@icloud.com>
2015-12-06 20:59:44 +0800

    update docs

--- cli.js
+++ cli.js
@@ -8,6 +8,18 @@ const argv = require('minimist')(process.argv.slice(2), { '--': true })
 if (argv.v || argv.version) {
   console.log(`${pkg.name.cyan} ~ ${pkg.version.magenta}`)
   process.exit()
+} else if (argv.h || argv.help) {
+  console.log(`
+  ${pkg.name.cyan} ~ ${pkg.version.magenta}
+
+  ${'Usages:'.white.underline}
+
+  mm -v/--version:         Print version
+  mm -h/--help:            Print docs
+  mm add [name]:           Add a time of a city/contry
+  mm:                      Print all available time sets
+`)
+  process.exit()
 }
 
 require('./')(argv)
